d14daf6396d056462d460eaf5922704a70d5aa08
Jonathan Kim <jonkim204@gmail.com>
2018-06-11 12:13:04 -0700

    3-5

--- jsfundamentals.js
+++ jsfundamentals.js
@@ -1,4 +1,11 @@
-const whoAmI = function(){
-	console.log ("Hi my name is Chris and I'm 29 years old");
+function yearOfBirth(age){
+	console.log(2018 - age);
 }
-whoAmI();
+
+const whoAmI = function(name, age){
+  let yearOfBirth = 2018 - age;
+  console.log (`Hi my ${name} is Chris and I'm ${age}years old`);
+  console.log(`I was born in ${yearOfBirth}`);
+};
+whoAmI(name, age);
+
