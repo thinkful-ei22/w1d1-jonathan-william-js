6057a5466c9be4b65de94bc0987339bf8ec507e9
Will Holcombe <william.holcombe@uconn.edu>
2018-06-11 16:21:18 -0400

    added beyond fn

--- functiondrills.js
+++ functiondrills.js
@@ -3,3 +3,21 @@ function jediName (firstName, lastName) {
 }
 
 jediName('Beyonce', 'Knowles');
+
+const beyond = function(num){
+	if(!isFinite(num)){
+		console.log("And beyond");
+
+	}
+	else if(isFinite(num) && num > 0){
+		console.log("To infinity");
+	}
+	else if(isFinite(num) && num < 0){
+		console.log("To negative infinity");
+	}
+	else{
+	console.log("Staying home");
+	}
+}
+beyond(0);
+beyond(-15);
