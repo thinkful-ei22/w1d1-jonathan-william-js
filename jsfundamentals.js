1f604fcb382d576cc86997d5988b2c0e0e3edb48
Will Holcombe <william.holcombe@uconn.edu>
2018-06-11 15:58:40 -0400

    added try catch and a few tests

--- jsfundamentals.js
+++ jsfundamentals.js
@@ -1,11 +1,31 @@
 function yearOfBirth(age){
-	console.log(2018 - age);
+	
+	
+	if(age < 1){
+  	throw new Error("Age can not be negative");
+  }
+
+  try{
+  	yearOfBirth(age);
+  }
+  catch (e){
+  	console.error(e.message);
+  }
+  console.log(2018 - age);
 }
 
+
 const whoAmI = function(name, age){
-  let yearOfBirth = 2018 - age;
-  console.log (`Hi my ${name} is Chris and I'm ${age}years old`);
+	if(typeof age !== 'number' || typeof name !== 'string'){
+		console.log("Incorrect input type")
+	}
+	if(!name || !age){
+		console.log("Arguments undefined")
+	}
+  let yob = yearOfBirth(age);
+  console.log (`Hi my ${name} is Chris and I'm ${age} years old`);
   console.log(`I was born in ${yearOfBirth}`);
+
 };
-whoAmI(name, age);
+whoAmI(5, 18);
 
