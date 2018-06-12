e95a8affa453606379eaf9abb36e36195ceb5fd0
Jonathan Kim <jonkim204@gmail.com>
2018-06-11 18:53:23 -0700

    attempt at solving the drills

--- functiondrills.js
+++ functiondrills.js
@@ -24,22 +24,67 @@ const beyond = function(num){
 beyond(0);
 beyond(-15);
 
-const decode = function (word) {
-  if (word[0] === 'a') {
-    console.log(word[1]);
-  }
-  else if (word[0] === 'b' ) {
-    console.log(word[2]);
-  }
-  else if (word[0] === 'c') {
-    console.log(word[3]);
-  }
-  else if (word[0] === 'd') {
-    console.log(word[4]);
-  }
-  else {
-	  console.log(' ');
-  }
-};
 
-decode('craft');
+//drill 3: cracking the code
+
+const message = 'craft block argon meter bells brown croon droop';
+
+// ['craft', 'block', 'argon', 'meter', 'bells', 'brown', 'croon', 'droop'];
+//loops i=7
+
+function decode(message){
+  let result = ''; //fo
+  let stringarr = message.split(' ');
+  for (let i=0; i < stringarr.length; i++) {
+    let firstLetter = stringarr[i][0];
+
+    switch(firstLetter) {
+    case 'a':
+      result += stringarr[1];
+      break;
+    case firstLetter === 'b':
+      result += stringarr[2];
+      break;
+    case firstLetter === 'c':
+      result += stringarr[3];
+      break;
+    case firstLetter === 'd':
+      result += stringarr[4];
+      break;
+    default:
+      result += '';
+      break;
+    }
+  }
+  return result;
+}
+
+// const myArray = message.split(' ');
+
+console.log(decode(message));
+
+// console.log(message.split(' '));
+
+function numberOfDays(month, leapYear) {
+
+  let result;
+  /*if (month !== 
+    throw 'Must provide a valid month.'
+  )*/
+  switch(month) {
+  case 'January':
+  case 'March':
+  case 'May':
+  case 'July':
+  case 'August':
+  case 'October':
+  case 'December':
+    result = 'This month has 31 days';
+    break;
+  case 'February':
+    result = 'This month has 28 days';
+  default:
+    result = 'this month has 30 days';
+  }
+  return result;
+}
